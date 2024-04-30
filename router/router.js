6ccd593eb4394ec833d3bfc3f31c0e6099f568c2
O-marin <omarancb@gmail.com>
2024-04-30 13:19:34 -0400

    desafio evaluado fbi system

---
+++ router/router.js
@@ -0,0 +1,11 @@
+import express from 'express'
+const router = express.Router()
+import { homeControl, inicioSesionControl,dashboardControl } from '../controllers/agentControl.js';
+
+
+router.get('/', homeControl);
+router.get('/signIn', inicioSesionControl);
+router.get('/dashboard',dashboardControl)
+
+
+export default router
